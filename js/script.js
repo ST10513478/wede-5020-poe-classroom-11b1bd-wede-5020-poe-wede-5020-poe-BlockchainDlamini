194ebd24b76fb98e693be94c9adcec6eb352dbf6
ST10513478 <ST10513478@rcconnect.edu.za>
2026-06-19 21:34:41 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ function addToCart(name, price) {
     let cart = JSON.parse(localStorage.getItem('cart')) || [];
     cart.push({name, price});
     localStorage.setItem('cart', JSON.stringify(cart));
-    alert(name + "added to cart!");
+    alert(name + " added to cart!");
 }
 
 document.querySelectorAll('.add-to-cart').forEach(button =>{
